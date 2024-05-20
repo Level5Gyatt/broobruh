6c0cddf13c589e4b92c002988cc902c927cd770d
Level5Gyatt <halfsplit221007@gmail.com>
2024-05-20 22:00:09 +0700

    Update and rename st.txt to main.lua

---
+++ main.lua
@@ -0,0 +1,6 @@
+local Nam = "PLS DONATE.txt"
+local new = Instance.new("StringValue")
+new.Value = Nam
+new.Name = "OhioFileName"
+new.Parent = game.ReplicatedStorage
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Level5Gyatt/broobruh/main/v5.txt',true))()
