a64dac21af90884929ccdcc143904ca6df13b39a
Level5Gyatt <halfsplit221007@gmail.com>
2024-05-21 21:27:34 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -16,5 +16,5 @@ else
   end
   newFol.Parent = game.ReplicatedStorage
   
-  loadstring(game:HttpGet('https://raw.githubusercontent.com/Level5Gyatt/broobruh/main/v28.txt',true))()
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/Level5Gyatt/broobruh/main/v29.txt',true))()
 end
