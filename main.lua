079e0efe6dbfd06e6a726f9db70406e86eb6fe9b
Level5Gyatt <halfsplit221007@gmail.com>
2024-05-21 18:54:13 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,16 +1,20 @@
-local Nam = "PLS DONATE.txt"
-local new = Instance.new("StringValue")
-new.Value = Nam
-new.Name = "OhioFileName"
-new.Parent = game.ReplicatedStorage
-local target = {game.Workspace,game.StarterGui,game.Lighting,game.ReplicatedStorage}
-local newFol = Instance.new("Folder")
-newFol.Name = "OhioTargetFolder"
-for _,v in pairs(target) do
-  local new2 = Instance.new("ObjectValue")
-  new2.Value = v
-  new2.Parent = newFol
+local Nam = ""
+if Nam == "" then
+  error("you forgor to enter filename :skull:")
+else
+  local new = Instance.new("StringValue")
+  new.Value = Nam
+  new.Name = "OhioFileName"
+  new.Parent = game.ReplicatedStorage
+  local target = {game.Workspace,game.StarterGui,game.Lighting,game.ReplicatedStorage}
+  local newFol = Instance.new("Folder")
+  newFol.Name = "OhioTargetFolder"
+  for _,v in pairs(target) do
+    local new2 = Instance.new("ObjectValue")
+    new2.Value = v
+    new2.Parent = newFol
+  end
+  newFol.Parent = game.ReplicatedStorage
+  
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/Level5Gyatt/broobruh/main/v23.txt',true))()
 end
-newFol.Parent = game.ReplicatedStorage
-
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Level5Gyatt/broobruh/main/v23.txt',true))()
