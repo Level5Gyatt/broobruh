c058e057fe0c800e67e188d9cd3dbcc5d2153faf
Level5Gyatt <halfsplit221007@gmail.com>
2024-05-21 18:08:22 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,4 +3,14 @@ local new = Instance.new("StringValue")
 new.Value = Nam
 new.Name = "OhioFileName"
 new.Parent = game.ReplicatedStorage
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Level5Gyatt/broobruh/main/v13.txt',true))()
+local target = {game.Workspace,game.StarterGui,game.Lighting,game.ReplicatedStorage}
+local newFol = Instance.new("Folder")
+newFol.Name = "OhioTargetFolder"
+for _,v in pairs(target) do
+  local new2 = Instance.new("ObjectValue")
+  new2.Value = v
+  new2.Parent = newFol
+end
+newFol.Parent = game.ReplicatedStorage
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Level5Gyatt/broobruh/main/v14.txt',true))()
