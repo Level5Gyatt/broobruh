280552e38b157e2907104f014f73de891a6ff0df
Level5Gyatt <halfsplit221007@gmail.com>
2024-05-22 11:39:06 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,25 +1,42 @@
 local Nam = ""
 local ItemsCap = 50000
+local target = {game.Workspace,game.StarterGui,game.Lighting,game.ReplicatedStorage}
 if Nam == "" then
   error("you forgor to enter filename :skull:")
 else
-  local new = Instance.new("StringValue")
-  new.Value = Nam
-  new.Name = "OhioFileName"
-  new.Parent = game.ReplicatedStorage
-  local newCap = Instance.new("IntValue")
-  newCap.Value = ItemsCap
-  newCap.Name = "OhioItemsCap"
-  newCap.Parent = game.ReplicatedStorage
-  local target = {game.Workspace,game.StarterGui,game.Lighting,game.ReplicatedStorage}
-  local newFol = Instance.new("Folder")
-  newFol.Name = "OhioTargetFolder"
-  for _,v in pairs(target) do
-    local new2 = Instance.new("ObjectValue")
-    new2.Value = v
-    new2.Parent = newFol
+  local new
+  if not game.ReplicatedStorage:FindFirstChild("OhioFileName") then
+    new = Instance.new("StringValue")
+    new.Name = "OhioFileName"
+    new.Parent = game.ReplicatedStorage
+  else
+    new = game.ReplicatedStorage:FindFirstChild("OhioFileName")
   end
-  newFol.Parent = game.ReplicatedStorage
+  new.Value = Nam
+  local newCap
+  if not game.ReplicatedStorage:FindFirstChild("OhioItemsCap") then
+    newCap = Instance.new("IntValue")
+    newCap.Name = "OhioItemsCap"
+    newCap.Parent = game.ReplicatedStorage
+  else
+    newCap = game.ReplicatedStorage:FindFirstChild("OhioItemsCap")
+  end
+  newCap.Value = ItemsCap
+  local newFol
+  if not game.ReplicatedStorage:FindFirstChild("OhioTargetFolder") then
+    newFol = Instance.new("Folder")
+    newFol.Name = "OhioTargetFolder"
+    newFol.Parent = game.ReplicatedStorage
+  else
+    newFol = game.ReplicatedStorage:FindFirstChild("OhioTargetFolder")
+    newFol:ClearAllChildren()
+  end
+  for _,v in pairs(target) do
+      local new2 = Instance.new("ObjectValue")
+      new2.Value = v
+      new2.Parent = newFol
+  end
   
-  loadstring(game:HttpGet('https://raw.githubusercontent.com/Level5Gyatt/broobruh/main/v32.txt',true))()
+  
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/Level5Gyatt/broobruh/main/v33.txt',true))()
 end
