ef3151add8c3f3e20ccc745f70cb8cfc62e0092a
Level5Gyatt <altrobloxacchalf@gmail.com>
2025-06-27 18:20:56 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,44 +1,45 @@
-local Nam = ""
+local Nam = "rate my avatar.txt"
 local ItemsCap = 50000
-local target = {game:GetService("Workspace"),game:GetService("StarterGui"),game:GetService("Lighting"),game:GetService("ReplicatedStorage")}
+local plr = game:GetService("Players").LocalPlayer
+local target = {game:GetService("Workspace"),plr.PlayerGui,game:GetService("Lighting"),game:GetService("ReplicatedStorage"),plr.Backpack}
 
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 if Nam == "" then
-	error("you forgor to enter filename :skull:")
+    error("you forgor to enter filename ðŸ’€")
 else
-	local new
-	if not ReplicatedStorage:FindFirstChild("OhioFileName") then
-		new = Instance.new("StringValue")
-		new.Name = "OhioFileName"
-		new.Parent = ReplicatedStorage
-	else
-		new = ReplicatedStorage:FindFirstChild("OhioFileName")
-	end
-	new.Value = Nam
-	local newCap
-	if not ReplicatedStorage:FindFirstChild("OhioItemsCap") then
-		newCap = Instance.new("IntValue")
-		newCap.Name = "OhioItemsCap"
-		newCap.Parent = ReplicatedStorage
-	else
-		newCap = ReplicatedStorage:FindFirstChild("OhioItemsCap")
-	end
-	newCap.Value = ItemsCap
-	local newFol
-	if not ReplicatedStorage:FindFirstChild("OhioTargetFolder") then
-		newFol = Instance.new("Folder")
-		newFol.Name = "OhioTargetFolder"
-		newFol.Parent = ReplicatedStorage
-	else
-		newFol = ReplicatedStorage:FindFirstChild("OhioTargetFolder")
-		newFol:ClearAllChildren()
-	end
-	for _,v in pairs(target) do
-		local new2 = Instance.new("ObjectValue")
-		new2.Value = v
-		new2.Parent = newFol
-	end
+    local new
+    if not ReplicatedStorage:FindFirstChild("OhioFileName") then
+        new = Instance.new("StringValue")
+        new.Name = "OhioFileName"
+        new.Parent = ReplicatedStorage
+    else
+        new = ReplicatedStorage:FindFirstChild("OhioFileName")
+    end
+    new.Value = Nam
+    local newCap
+    if not ReplicatedStorage:FindFirstChild("OhioItemsCap") then
+        newCap = Instance.new("IntValue")
+        newCap.Name = "OhioItemsCap"
+        newCap.Parent = ReplicatedStorage
+    else
+        newCap = ReplicatedStorage:FindFirstChild("OhioItemsCap")
+    end
+    newCap.Value = ItemsCap
+    local newFol
+    if not ReplicatedStorage:FindFirstChild("OhioTargetFolder") then
+        newFol = Instance.new("Folder")
+        newFol.Name = "OhioTargetFolder"
+        newFol.Parent = ReplicatedStorage
+    else
+        newFol = ReplicatedStorage:FindFirstChild("OhioTargetFolder")
+        newFol:ClearAllChildren()
+    end
+    for _,v in pairs(target) do
+        local new2 = Instance.new("ObjectValue")
+        new2.Value = v
+        new2.Parent = newFol
+    end
 
 
-	loadstring(game:HttpGet('https://raw.githubusercontent.com/Level5Gyatt/broobruh/main/v39.txt',true))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Level5Gyatt/broobruh/main/v49.txt',true))()
 end
